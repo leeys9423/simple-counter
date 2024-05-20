14b95f0caf9ffdc4d5826db8529ab83cd94a1cc6
yslee <leeys9423@gmail.com>
2024-05-20 21:51:00 +0900

    ✨ useState 실습

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,34 @@
 import "./App.css";
 import Viewer from "./components/Viewer";
 import Controller from "./components/Controller";
-import { useState } from "react";
+import Even from "./components/Even";
+import { useState, useEffect, useRef } from "react";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [input, setInput] = useState("");
+
+  const isMount = useRef(false);
+
+  // 1. 마운트 : 탄생(빈 배열로 선언하기)
+  useEffect(() => {
+    console.log("mount");
+  }, []);
+
+  // 2. 업데이트 : 변화, 리렌더링(배열 생략하기)
+  useEffect(() => {
+    if (!isMount.current) {
+      isMount.current = true;
+      return;
+    }
+    console.log("update");
+  });
+
+  // 3. 언마운트 : 죽음
+
+  // 의존성 배열
+  // dependency array
+  // deps
 
   const onClickButton = (value) => {
     setCount(count + value);
@@ -14,7 +38,16 @@ function App() {
     <div className="App">
       <h1>Simple Counter</h1>
       <section>
+        <input
+          value={input}
+          onChange={(e) => {
+            setInput(e.target.value);
+          }}
+        />
+      </section>
+      <section>
         <Viewer count={count} />
+        {count % 2 === 0 ? <Even /> : null}
       </section>
 
       <section>
